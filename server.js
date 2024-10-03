9a6d16fb26a382719804a7eb9153dd2a505ccaa3
dlubenchenko <d.lubenchenko@gmail.com>
2024-10-03 19:39:22 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const mongoUri = process.env.MONGODB_URI;
+
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error(err));
+
+const contactSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    phone: String,
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+// API routes
+app.get('/contacts', async (req, res) => {
+    const contacts = await Contact.find();
+    res.json(contacts);
+});
+
+app.post('/contacts', async (req, res) => {
+    const newContact = new Contact(req.body);
+    await newContact.save();
+    res.json(newContact);
+});
+
+app.put('/contacts/:id', async (req, res) => {
+    const updatedContact = await Contact.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updatedContact);
+});
+
+app.delete('/contacts/:id', async (req, res) => {
+    await Contact.findByIdAndDelete(req.params.id);
+    res.sendStatus(204);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
